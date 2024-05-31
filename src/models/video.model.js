dd3d88b90deea0ae572ade5c517081868cb2d79b
shobhit1502 <shobhit.bansal.150202@gmail.com>
2024-05-31 16:51:37 +0530

    video model changes

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,94 +1,47 @@
-
 import mongoose, {Schema} from "mongoose";
-import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const userSchema = new Schema(
+const videoSchema = new Schema(
     {
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-            lowercase: true,
-            trim: true, 
-            index: true
+        videoFile: {
+            type: String, //cloudinary url
+            required: true
         },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            lowecase: true,
-            trim: true, 
+        thumbnail: {
+            type: String, //cloudinary url
+            required: true
         },
-        fullName: {
-            type: String,
-            required: true,
-            trim: true, 
-            index: true
+        title: {
+            type: String, 
+            required: true
         },
-        avatar: {
-            type: String, // cloudinary url
-            required: true,
+        description: {
+            type: String, 
+            required: true
         },
-        coverImage: {
-            type: String, // cloudinary url
+        duration: {
+            type: Number, 
+            required: true
         },
-        watchHistory: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: "Video"
-            }
-        ],
-        password: {
-            type: String,
-            required: [true, 'Password is required']
+        views: {
+            type: Number,
+            default: 0
         },
-        refreshToken: {
-            type: String
+        isPublished: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
         }
 
-    },
+    }, 
     {
         timestamps: true
     }
 )
 
-userSchema.pre("save", async function (next) {
-    if(!this.isModified("password")) return next();
+videoSchema.plugin(mongooseAggregatePaginate)
 
-    this.password = await bcrypt.hash(this.password, 10)
-    next()
-})
-
-userSchema.methods.isPasswordCorrect = async function(password){
-    return await bcrypt.compare(password, this.password)
-}
-
-userSchema.methods.generateAccessToken = function(){
-    return jwt.sign(
-        {
-            _id: this._id,
-            email: this.email,
-            username: this.username,
-            fullName: this.fullName
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        {
-            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-        }
-    )
-}
-userSchema.methods.generateRefreshToken = function(){
-    return jwt.sign(
-        {
-            _id: this._id,
-            
-        },
-        process.env.REFRESH_TOKEN_SECRET,
-        {
-            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
-        }
-    )
-}
-
-export const User = mongoose.model("User", userSchema)
+export const Video = mongoose.model("Video", videoSchema)
